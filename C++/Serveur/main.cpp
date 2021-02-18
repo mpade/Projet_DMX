ad1fb22f8916d4b5c20ee55dd27eae1b785ea34b
debian <debian@debian10Server.lapro.local>
2021-02-18 10:44:57 +0100

    TCPserver en construction linux

---
+++ C++/Serveur/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "serverTCP.h"
+using namespace std;
+
+int main()
+{
+    serverTCP *TCPs = new serverTCP();
+    int test = TCPs->createSocket(502);
+    cout << test << endl;
+    cout << "Hello world!" << endl;
+    system("PAUSE");
+    return 0;
+}
