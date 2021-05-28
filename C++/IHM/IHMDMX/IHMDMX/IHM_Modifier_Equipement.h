35aca69bbef98da47eca9baeccf6ac6bbc766367
Alecouflet <alecouflet@la-providence.net>
2021-05-28 15:46:09 +0200

    Revert "Modifier Equipement"
    This reverts commit 89b0f8217328f4eb6242eef59f97b24bb4d71ad9.

--- C++/IHM/IHMDMX/IHMDMX/IHM_Modifier_Equipement.h
+++ C++/IHM/IHMDMX/IHMDMX/IHM_Modifier_Equipement.h
@@ -21,54 +21,13 @@ class IHM_Modifier_Equipement : public QWidget
 	Q_OBJECT                    // Nécessaire pour créer un slot maison
 
 public:
-	IHM_Modifier_Equipement();
+
 
 public slots:					// Slots maison
-
-	void Connexionbdd();
-	void Modif_Validation();
-	void Placeholder();
-	void getAllEquipement();
-	void Refresh();
 
 
 private:
 
-	QLineEdit *LE_Modif_Name;
-	QLineEdit *LE_Modif_Voies;
-	QLineEdit *LE_Modif_AdressEquipement;
-
-	QLabel *L_Name;
-	QLabel *L_Voies;
-	QLabel *L_AdressEquipement;
-	QLabel *m_LConnexionBdd;
-
-	QString Name;
-	QString Voies;
-	QString AdressEquipement;
-	QString Old_Name;
-	QString Old_Voies;
-	QString Old_AdressEquipement;
-	std::string id_AdressEquipement;
-
-	QPushButton *B_Modif_Valider;
-	QPushButton *refresh;
-
-	QListWidget *ListAfficherEquipement;
-
-	std::vector<QListWidgetItem*>e;
-
-	QGridLayout *grid2;
-
-	MYSQL *mySQL;
-
-	MYSQL_ROW row = NULL;
-	MYSQL_ROW rows = NULL;
-	MYSQL_ROW row1 = NULL;
-	MYSQL_ROW row2 = NULL;
-	MYSQL_RES *result = NULL;
-	MYSQL_RES *results = NULL;
-	MYSQL_RES *result1 = NULL;
-	MYSQL_RES *result2 = NULL;
 
 };
+
